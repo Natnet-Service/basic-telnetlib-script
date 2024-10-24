69f6f4479d0232065bc57098ae90c0d54efb218f
Natnet-Service <nathy.network@gmail.com>
2024-10-24 16:15:43 +0000

    Add files via upload

---
+++ multiple vlan on multiple switchs copy.py
@@ -0,0 +1,34 @@
+import telnetlib
+import getpass
+
+# Define important variables
+
+user = input('enter your usename:  ')
+password = getpass.getpass()
+
+
+for h in range (10,14):
+    HOST = '192.168.158.' + str(h)
+    print('connecting to host' + HOST)
+    
+    # Using this information for telnet into device
+    tn = telnetlib. Telnet(HOST)
+
+    # Handle the Username/Password prompt and supply our values
+    tn.read_until(b"Username: ")
+    tn.write(user.encode('ascii') + b"\n")
+    if password:
+        tn.read_until(b"Password: ")
+        tn.write(password.encode('ascii') + b'\n')
+
+    # Write the commands on the device
+
+    tn.write(b'conf ter\n') #user priviledge is set to 15. that is why you dont need to input enable pass
+    for n in range (2,101):
+        k = str(n)
+        tn.write(b'vlan ' + k.encode('ascii') +b'\n')
+        tn.write(b'name vlan_ ' + k.encode('ascii') +b'\n')
+    tn.write(b'end\n')
+    tn.write(b'exit\n')
+
+    print(tn.read_all().decode())
